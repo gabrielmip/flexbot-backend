aba421413cddcd53c4693864675069bacbfb4272
Gabriel Pedrosa <gabriel.pedrosa@waycarbon.com>
2019-03-10 00:34:37 -0300

    AccessToken table to the model

--- flexbot/models.py
+++ flexbot/models.py
@@ -1,13 +1,16 @@
+import datetime
 from database import MyModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import (
-    Column, Integer, String, MetaData, ForeignKey
+    Column, Integer, String, MetaData, ForeignKey, DateTime
 )
+
 
 class Chat(MyModel):
     __tablename__ = 'chat'
     chat_id = Column(Integer, primary_key=True)
     title = Column(String(100))
+
 
 class User(MyModel):
     __tablename__ = 'user'
@@ -15,23 +18,40 @@ class User(MyModel):
     first_name = Column(String(100))
     user_name = Column(String(100))
 
+
 class Message(MyModel):
     __tablename__ = 'message'
     message_id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, ForeignKey(Chat.chat_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
-    user_id = Column(Integer, ForeignKey(User.user_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    chat_id = Column(Integer, ForeignKey(
+        Chat.chat_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    user_id = Column(Integer, ForeignKey(
+        User.user_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
     text = Column(String(400))
+
 
 class Trigger(MyModel):
     __tablename__ = 'trigger'
     trigger_id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, ForeignKey(Chat.chat_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    chat_id = Column(Integer, ForeignKey(
+        Chat.chat_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
     expression = Column(String(4096))
 
     answers = relationship("Answer")
 
+
 class Answer(MyModel):
     __tablename__ = 'answer'
     answer_id = Column(Integer, primary_key=True)
-    trigger_id = Column(Integer, ForeignKey(Trigger.trigger_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    trigger_id = Column(Integer, ForeignKey(
+        Trigger.trigger_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
     text = Column(String(4096))
+
+
+class AccessToken(MyModel):
+    __tablename__ = 'access_token'
+    token = Column(String(50))
+    chat_id = Column(Integer, ForeignKey(
+        Chat.chat_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    user_id = Column(Integer, ForeignKey(
+        User.user_id, onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
