632b78c9452920b48cb22913b693f15d873897c8
Gabriel Pedrosa <gabriel@Gabriels-iMac.local>
2019-04-06 22:28:42 -0300

    Implemented trigger creation

--- config_panel/forms/edit_trigger.py
+++ config_panel/forms/edit_trigger.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import (
-    StringField, SubmitField, TextAreaField)
+    StringField, SubmitField, TextAreaField, HiddenField)
 
 
 class EditTrigger(FlaskForm):
     expression = StringField('Trigger', validators=[DataRequired()])
     answers = TextAreaField('Answers', validators=[DataRequired()])
+    chat_id = HiddenField('chat_id')
     submit = SubmitField('Save')
--- config_panel/views.py
+++ config_panel/views.py
@@ -29,18 +29,21 @@ def list_triggers():
         'list_triggers.html', token=received_token, chat=access_token.chat)
 
 
+@config_panel.route('/triggers/', methods=['get', 'post'])
 @config_panel.route('/triggers/<trigger_id>', methods=['get', 'post'])
-def edit_trigger(trigger_id):
+def edit_trigger(trigger_id = None):
     token = request.args.get('token')
     form = EditTrigger()
+    trigger = (create_new_trigger_from_token(token)
+        if trigger_id is None
+        else Trigger.query.get(trigger_id))
 
     if form.validate_on_submit():
-        update_trigger_from_form(trigger_id, form)
+        update_trigger_from_form(trigger, form)
         list_triggers_url = url_for('config_panel.list_triggers', token=token)
         return redirect(list_triggers_url)
 
     else:
-        trigger = Trigger.query.get(trigger_id)
         joined_answers = '\n'.join([a.text.strip() for a in trigger.answers])
         form = EditTrigger(
             expression=trigger.expression, answers=joined_answers)
@@ -48,11 +51,15 @@ def edit_trigger(trigger_id):
             'edit_trigger.html', form=form, trigger_id=trigger_id, token=token)
 
 
-def update_trigger_from_form(trigger_id, form):
-    current_trigger = Trigger.query.get(trigger_id)
+def update_trigger_from_form(current_trigger, form):
     current_trigger.update(expression=form.expression.data.strip())
 
     for answer in current_trigger.answers:
         answer.delete()
     for text in form.answers.data.splitlines():
-        Answer(text=text, trigger_id=trigger_id).save()
+        Answer(text=text, trigger_id=current_trigger.trigger_id).save()
+
+
+def create_new_trigger_from_token(token):
+    access_token = AccessToken.find(token)
+    return Trigger(chat_id=access_token.chat.chat_id)
