5914bf2fe1e911f663a094d2f4949dd6c4af41b7
Gabriel Pedrosa <gabriel@Gabriels-iMac.local>
2019-06-01 22:48:22 -0300

    Implementing trigger group CRUD

--- config_panel/views.py
+++ config_panel/views.py
@@ -19,9 +19,6 @@ config_panel = Blueprint(
 
 @config_panel.route('/<received_token>')
 def get_chat_associated_to_token(received_token):
-    if received_token is None:
-        return "Invalid token", 401
-
     access_token = AccessToken.find(received_token)
     if access_token is None:
         return "Invalid token", 401
@@ -29,58 +26,76 @@ def get_chat_associated_to_token(received_token):
     return jsonify(access_token.chat.to_dict())
 
 
-@config_panel.route('/triggers/', methods=['get', 'post'])
-@config_panel.route('/triggers/<trigger_id>', methods=['get', 'post'])
-def edit_trigger(trigger_id=None):
-    token = request.args.get('token')
-    trigger = (create_new_trigger_from_token(token)
-        if trigger_id is None
-        else Trigger.query.get(trigger_id))
+@config_panel.route('/trigger_groups', methods=['post'])
+@config_panel.route('/trigger_groups/<trigger_group_id>', methods=['put'])
+def edit_trigger_group(trigger_group_id=None):
+    received_token = request.headers.get('Authorization')
+    access_token = AccessToken.find(received_token)
+    if access_token is None:
+        return "Invalid token", 401
 
-    form = EditTrigger()
-    if form.validate_on_submit():
-        update_trigger_from_form(trigger, form)
-        return redirect_to_list(token)
+    trigger_group = (TriggerGroup(chat_id=access_token.chat_id)
+        if trigger_group_id is None
+        else TriggerGroup.query.get(trigger_group_id))
 
-    joined_answers = '\n'.join([a.text.strip() for a in trigger.answers])
-    form = EditTrigger(
-        expression=trigger.expression, answers=joined_answers)
-    return render_template(
-        'edit_trigger.html', form=form, trigger_id=trigger_id, token=token)
+    if trigger_group is None:
+        return "Not found", 404
+    
+    if trigger_group.chat_id != access_token.chat_id:
+        return "Forbidden", 403
+
+    group_attrs = request.get_json()
+    trigger_group.update(ignore_case=group_attrs['ignore_case'], ignore_repeated_letters=group_attrs['ignore_repeated_letters'])
+    update_trigger_group_attrs(trigger_group, group_attrs['answers'], group_attrs['triggers'])
+
+    return "saved"
 
 
-@config_panel.route('/triggers/<trigger_id>/delete', methods=['post'])
-def delete_trigger(trigger_id):
-    token = request.args.get('token')
+@config_panel.route('/trigger_groups/<trigger_group_id>', methods=['delete'])
+def delete_trigger_group(trigger_group_id):
+    token = request.headers.get('Authorization')
     access_token = AccessToken.find(token)
-    trigger = Trigger.query.get(trigger_id)
+    trigger_group = TriggerGroup.query.get(trigger_group_id)
 
-    if trigger.chat_id != access_token.chat.chat_id:
-        return render_template('invalid_token.html'), 403
+    if access_token is None:
+        return "Invalid", 401
 
-    delete_trigger_answers(trigger)
-    trigger.delete()
-    return redirect_to_list(token)
+    if trigger_group is None:
+        return "Not found", 404
 
+    if trigger_group.chat_id != access_token.chat_id:
+        return "Forbidden", 403
+    
+    delete_answers(trigger_group)
+    delete_triggers(trigger_group)
+    trigger_group.delete()
 
-def redirect_to_list(token):
-    list_triggers_url = url_for('config_panel.list_triggers', token=token)
-    return redirect(list_triggers_url)
-
-
-def update_trigger_from_form(current_trigger, form):
-    current_trigger.update(expression=form.expression.data.strip())
-
-    delete_trigger_answers(current_trigger)
-    for text in form.answers.data.splitlines():
-        Answer(text=text, trigger_id=current_trigger.trigger_id).save()
+    return "deleted"
 
 
-def create_new_trigger_from_token(token):
-    access_token = AccessToken.find(token)
-    return Trigger(chat_id=access_token.chat.chat_id)
+def update_trigger_group_attrs(trigger_group, updated_answers, updated_triggers):
+    update_answers(trigger_group, updated_answers)
+    update_triggers(trigger_group, updated_triggers)
 
 
-def delete_trigger_answers(trigger):
-    for answer in trigger.answers:
-        answer.delete()
+def update_answers(trigger_group, new_answers):
+    delete_answers(trigger_group)
+    for new_answer in new_answers:
+        Answer(text=new_answer['text'], trigger_group_id=trigger_group.trigger_group_id).save()
+
+
+def delete_answers(trigger_group):
+    for current_answer in trigger_group.answers:
+        current_answer.delete()
+
+    
+def update_triggers(trigger_group, new_triggers):
+    delete_triggers(trigger_group)
+    for new_trigger in new_triggers:
+        Trigger(expression=new_trigger['expression'], trigger_group_id=trigger_group.trigger_group_id).save()
+
+
+def delete_triggers(trigger_group):
+    for current_trigger in trigger_group.triggers:
+        current_trigger.delete()
+    
